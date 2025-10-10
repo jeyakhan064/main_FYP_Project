72be5fadaeb0abdb394d3e611307faf54adf23f0
Sabahat Farhat <sabahatfarhat1418@gmail.com>
2025-10-10 21:30:27 +0500

    fix: width of front card , added background image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,28 @@
-import { Link } from 'react-router-dom';
-import { useSnapshot } from 'valtio';
-import state from '../store';
-import Canvas from '../canvas';
-import Home from '../components/Home';
-import Customizer from './Customizer';
-import Header from '../components/Header';
+import { useSnapshot } from "valtio";
+import state from "../store";
+import Canvas from "../canvas";
+import Home from "../components/Home";
+import Customizer from "./Customizer";
+import Header from "../components/Header";
+
 const HomePage = () => {
   const snap = useSnapshot(state);
 
   return (
-    <>
-    <main className="app transition-all ease-in">
+    <main className="app transition-all ease-in relative">
       <Header />
-      <Home />
-      <Canvas />
-      <Customizer />
+
+      {/* Show Home only when intro is true */}
+      {snap.intro ? (
+        <Home />
+      ) : (
+        <>
+          {/* Show 3D Canvas and Customizer only after "Let's Do It" */}
+          <Canvas />
+          <Customizer />
+        </>
+      )}
     </main>
-    </>
   );
 };
 
